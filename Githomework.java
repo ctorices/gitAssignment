c81d6d1dc18d138ab084adebdfa40308ea9d7ae4
styles04 <astyles@emich.edu>
2016-03-02 13:28:03 -0500

    Update Githomework.java

--- Githomework.java
+++ Githomework.java
@@ -1,97 +1,129 @@
 /**
  * Group Memebers: 
  * Christian Torices
+ * Stephen Maynard
+ * Drew Styles
  * 
+ *
  * Homework 3: Git Hub Assignment
  * COSC 481W
  * 2/18/2016
  * 
  */
-package githomework;
+//package githomework;  // (┛◉Д◉)┛彡ǝsdᴉlɔƎ
 import java.util.*;
 
 public class Githomework {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-       
         display();
-        
-        
-        
     }
-     public static enum game {
+
+    public static enum game {
         ROCK, 
         PAPER, 
         SCISSOR, 
-        LIZZARD, 
+        LIZARD, 
         SPOCK,
-        QUIT
+        QUIT,
+        ERROR
     }
+
+    public static boolean contains(String test) {
+        for (game g : game.values()) {
+            if (g.name().equals(test)) {
+                return true;
+            }
+        }
+       return false;
+    }
+
      public static game input() {
-         game temp = game.LIZZARD;
+         game temp = game.ERROR;
          Scanner keyboard = new Scanner(System.in);
-         temp = game.valueOf(keyboard.next().toUpperCase());
-         //keyboard.close();
+         String str = keyboard.next().toUpperCase();
+         if(contains(str))
+             temp = game.valueOf(str);
          return temp;
      }
     public static void choice(game choice){
       Random rand = new Random();
-      rand.setSeed(0);
-    
-      
+
         game aiChoice;
-        aiChoice = game.values()[rand.nextInt((3-0) + 1)+ 0];
-       
+        aiChoice = game.values()[rand.nextInt(5)];
+        System.out.println("Computer: "+aiChoice);
+
         switch(choice) {
             case ROCK:
-                if(aiChoice == game.ROCK)
-                    System.out.println("Tie");
-                else if(aiChoice == game.PAPER)
-                    System.out.println("Lose");
-                else if(aiChoice == game.SCISSOR)
+                if(aiChoice == game.SCISSOR || aiChoice == game.LIZARD)
                     System.out.println("Win");
-         
+                else if(aiChoice == game.PAPER || aiChoice == game.SPOCK)
+                    System.out.println("Lose");
+                else if(aiChoice == game.ROCK)
+                    System.out.println("Tie");
                 break;
+
             case PAPER:
-                if(aiChoice == game.ROCK)
+                if(aiChoice == game.ROCK || aiChoice == game.SPOCK)
                     System.out.println("Win");
+                else if(aiChoice == game.SCISSOR || aiChoice == game.LIZARD)
+                    System.out.println("Lose");
                 else if(aiChoice == game.PAPER)
                     System.out.println("Tie");
-                else if(aiChoice == game.SCISSOR)
-                    System.out.println("Lose");
                 break;
+
             case SCISSOR:
-                if(aiChoice == game.ROCK)
-                    System.out.println("Lose");
-                else if(aiChoice == game.PAPER)
+                if(aiChoice == game.PAPER || aiChoice == game.LIZARD)
                     System.out.println("Win");
+                else if(aiChoice == game.ROCK || aiChoice == game.SPOCK)
+                    System.out.println("Lose");
                 else if(aiChoice == game.SCISSOR)
                     System.out.println("Tie");
-                break;               
+                break;
+
+            case LIZARD:
+                if(aiChoice == game.PAPER || aiChoice == game.SPOCK)
+                    System.out.println("Win");
+                else if(aiChoice == game.ROCK || aiChoice == game.SCISSOR)
+                    System.out.println("Lose");
+                else if(aiChoice == game.LIZARD)
+                    System.out.println("Tie");
+                break;
+
+            case SPOCK:
+                if(aiChoice == game.ROCK || aiChoice == game.SCISSOR)
+                    System.out.println("Win");
+                else if(aiChoice == game.PAPER || aiChoice == game.LIZARD)
+                    System.out.println("Lose");
+                else if(aiChoice == game.SPOCK)
+                    System.out.println("Tie");
+                break;
+
+            case QUIT:
+                System.exit(0);
+                break;
+
             default:
                 System.out.println("Nope not a option");
                 break;
         }
         
     }
-   
+
     public static void display(){
         game isDone;
             
         while(true){
-            System.out.println("Rock Paper Scissor Lizzard Spock");
+            System.out.println("================================\nRock Paper Scissor Lizard Spock\n================================\n");
             System.out.println("Menu:");
             System.out.println("Rock");
             System.out.println("Paper");
             System.out.println("Scissor");
-            System.out.println("Quit");
+            System.out.println("Lizard");
+            System.out.println("Spock");
+            System.out.println("Quit\n");
             isDone = input();
-           if( isDone == game.QUIT)
-               break;
+                             
            choice(isDone);
         }
     }
